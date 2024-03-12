51b8ccf1e63a8490157727504a1dd17f3577b350
Cristian Correa <cristian.correa@unl.edu.ec>
2024-03-11 23:22:04 -0500

    commits

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    name = models.CharField(max_length=50)
+    curso = models.CharField(max_length=3)
+    asignatura = models.CharField(max_length=100)  
+    docente = models.CharField(max_length=50)  
+    date = models.DateTimeField()
+    asistencia = models.BooleanField(default=False)  
+
+    class Meta:
+        db_table = 'estudiante'
+
